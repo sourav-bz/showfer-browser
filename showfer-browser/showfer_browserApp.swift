95a52832ced7eb9271f492685f34c36233918e11
sourav-bz <s.kumar001997@gmail.com>
2024-10-14 21:11:59 +0530

    Initial Commit

---
+++ showfer-browser/showfer_browserApp.swift
@@ -0,0 +1,17 @@
+//
+//  showfer_browserApp.swift
+//  showfer-browser
+//
+//  Created by Sourav on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct showfer_browserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ showfer-browserTests/showfer_browserTests.swift
@@ -0,0 +1,17 @@
+//
+//  showfer_browserTests.swift
+//  showfer-browserTests
+//
+//  Created by Sourav on 14/10/24.
+//
+
+import Testing
+@testable import showfer_browser
+
+struct showfer_browserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
